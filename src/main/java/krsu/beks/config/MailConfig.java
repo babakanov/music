df4e6b53cf9a63d894a3089f89a1e6f37a229ecf
babakanov <babakanovb@gmail.com>
2020-07-08 01:08:52 +0600

    Изменения сделаны

---
+++ src/main/java/krsu/beks/config/MailConfig.java
@@ -0,0 +1,54 @@
+package krsu.beks.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Value("${mail.debug}")
+    private String debug;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private Boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private Boolean enable;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(465);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", enable);
+        props.put("mail.debug", debug);
+
+        return mailSender;
+    }
+}
